5451ef8b631a53a855155b02ed8beb3bb93e7c84
cesarcacho <cesarcacho@gmail.com>
2021-03-17 11:59:43 +0100

    tilt checed

--- js/sripts.js
+++ js/sripts.js
@@ -1,15 +1,21 @@
-function insertListItem() {
-  const items = document.querySelectorAll('li');
+function insertListItem(task) {
+  const items = document.querySelectorAll('li.list_item-container');
   const numberOfItems = items.length;
   const lastItems = items[numberOfItems - 1];
-  lastItems.insertAdjacentHTML('afterend','<li>nuevo elemento</li>');
-  debugger;  
-};
+  lastItems.insertAdjacentHTML('afterend', `<div class="list_item-container"><li class="li_element">${task}</li><span>X</span></div>`);
+}
 
 
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('#todo_form');
   form.addEventListener('submit', function() {
-    insertListItem();
+    const text = document.querySelector("input[type='text']");
+    const deleteElements = document.querySelectorAll('.list_item-container span');
+
+
+    
+    insertListItem(text.value);
+    text.value = '';
+    event.preventDefault();
   });
 });
