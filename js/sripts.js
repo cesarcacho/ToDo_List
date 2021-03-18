3a09334a902dc432ebe1bf5833627e89ca357942
cesarcacho <cesarcacho@gmail.com>
2021-03-18 13:13:05 +0100

    add & delete element

--- js/sripts.js
+++ js/sripts.js
@@ -1,21 +1,46 @@
 function insertListItem(task) {
-  const items = document.querySelectorAll('li.list_item-container');
-  const numberOfItems = items.length;
-  const lastItems = items[numberOfItems - 1];
-  lastItems.insertAdjacentHTML('afterend', `<div class="list_item-container"><li class="li_element">${task}</li><span>X</span></div>`);
+  const newItem = document.createElement('div');
+  newItem.classList.add('list_item-container');
+  newItem.innerHTML = `<li class="list_element list_element-completed">${task}</li><span>X</span>`;
+ 
+  console.log(newItem);
+  const list = document.querySelector('ul');
+  list.appendChild(newItem);
+  markItemAsCompletedListener(newItem);
+  deleteItemListener(element);
 }
 
+function markItemAsCompletedListener(element){
+  element.addEventListener('click', function(event){
+    const element = event.target;
+    element.classList.toggle('list_element-completed');
+  });
+};
 
-window.addEventListener('DOMContentLoaded', function() {
+function deleteItemListener(element) {
+  element.addEventListener('click', function (event) {
+    const item = event.target;
+    item.parentElement.remove();
+  });
+};
+
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('#todo_form');
-  form.addEventListener('submit', function() {
-    const text = document.querySelector("input[type='text']");
-    const deleteElements = document.querySelectorAll('.list_item-container span');
+  const textInput = document.querySelector("input[type='text']");
+  const listItem = document.querySelectorAll('.list_element');
+  const deleteElements = document.querySelectorAll('.list_item-container span');
 
+  listItem.forEach(function(element){
+    markItemAsCompletedListener(element);
+  });
 
-    
-    insertListItem(text.value);
-    text.value = '';
+  deleteElements.forEach(function (element) {
+    deleteItemListener(element);
+  });
+
+  form.addEventListener('submit', function () {
+    insertListItem(textInput.value);
+    textInput.value = '';
     event.preventDefault();
   });
 });
