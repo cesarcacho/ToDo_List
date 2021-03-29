7c63cfdb1bc096a2d22089dba7f94b8a20617bc1
cesarcacho <cesarcacho@gmail.com>
2021-03-29 15:07:24 +0200

    final result

--- js/sripts.js
+++ js/sripts.js
@@ -1,49 +1,58 @@
 function insertListItem(task) {
   const newItem = document.createElement('li');
   const newItemDeleteButton = document.createElement('span');
+  const newItemCheckbox = document.createElement('input');
 
-  newItemDeleteButton.textContent = 'X';
+  newItemDeleteButton.textContent = '❌';
   newItem.classList.add('list_element');
   newItem.innerHTML = `${task}`;
   
- 
-  console.log(newItem);
+  newItemCheckbox.type = 'checkbox';
+
   const list = document.querySelector('ul');
   list.appendChild(newItem);
-  markItemAsCompletedListener(newItem);
 
+  newItem.prepend(newItemCheckbox);
   newItem.appendChild(newItemDeleteButton);
   deleteItemListener(newItemDeleteButton);
+  markItemAsCompletedListener(newItemCheckbox);
 }
 
-function markItemAsCompletedListener(element){
-  element.addEventListener('click', function(event){
-    const element = event.target;
-    element.classList.toggle('list_element-completed');
+function markItemAsCompletedListener(element) {
+  element.addEventListener('click', function(){
+    toggleElementAsCompleted(element.parentElement);
   });
-};
+}
+
 
 function deleteItemListener(element) {
   element.addEventListener('click', function(event) {
     const item = event.target;
     item.parentElement.remove();
   });
-};
+}
+
+function toggleElementAsCompleted(element){
+  const checkbox = element.querySelector('input[type="checkbox"]');
+  element.classList.toggle('list_element-completed');
+}
 
 window.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('#todo_form');
-  const textInput = document.querySelector("input[type='text']");
-  const listItem = document.querySelectorAll('.list_element');
+  const textInput = document.querySelector('input[type="text"]');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   const deleteElements = document.querySelectorAll('.list_element');
 
-  listItem.forEach(function(element){
-    markItemAsCompletedListener(element);
-  });
+  checkboxes.forEach(function(element){
+    element.addEventListener('change', function() {
+      toggleElementAsCompleted(element.parentElement);
+    });
+  })
 
   deleteElements.forEach(function (element) {
     const deleteItemButton = element.querySelector('span');
     deleteItemListener(deleteItemButton);
-  });
+  })
 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
